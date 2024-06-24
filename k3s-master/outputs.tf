a1518d5fb1e3ac73f6cc3a6868c669f86d26bfc8
OleksDovz <oleksdovz@gmail.com>
2024-06-24 14:32:16 +0300

    fix(DMP-000):  Update file: k3s-master/main.tf Update file: k3s-master/outputs.tf

--- k3s-master/outputs.tf
+++ k3s-master/outputs.tf
@@ -1,9 +1,4 @@
-# output "k3s_config" {
-#   value     = local.k3s_config
-#   sensitive = false
-# }
-
-output "ssh_private_key" {
-  value     = var.ssh_private_key
+output "k3s_config" {
+  value     = local.k3s_config
   sensitive = false
 }
